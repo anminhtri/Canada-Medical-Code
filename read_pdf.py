9daa00630be1eeac7f09ec72753360cc1adf37bb
An Minh Tri <minhtri5813@gmail.com>
2025-10-10 22:30:22 -0400

    add table extraction and change text extraction algorithm

--- read_pdf.py
+++ read_pdf.py
@@ -1,14 +1,36 @@
-from pypdf import PdfReader as pdf
 import numpy as np
+import pdfplumber
 
 path = "2022 CCI Alphabetic and Tabular List.pdf"
 pages = range(68, 69)
 
 def readpdf(path, pages):   
-    text = np.array([])
+    texts = []
     pages = range(pages.start - 1, pages.stop)
-    read = pdf(path)
-    subset = [read.pages[i] for i in pages]
-    for page in subset:
-        text = np.append(text, page.extract_text())
-    return text
+    with pdfplumber.open(path) as pdf:
+        for i in pages:
+            page = pdf.pages[i]
+            text = page.extract_text()
+            texts.append(text)
+    return texts
+
+# Test
+# text = readpdf(path, pages)   
+# print(text)
+
+def readtable(path, pages):
+    tables_list = []
+    pages = range(pages.start - 1, pages.stop)
+    with pdfplumber.open(path) as pdf:
+        for i in pages:  
+            page = pdf.pages[i]
+            tables = page.extract_tables()
+            for table in tables:
+                tables_list.append(np.array(table))
+    return tables_list
+    
+# Test
+# tables_list = readtable(path, pages)
+# if tables_list:
+#     print(tables_list)
+# else: print("no table")
